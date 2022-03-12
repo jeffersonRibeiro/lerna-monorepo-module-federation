d522b60ce73bc46da1e255bd6830d25cb1d0fad3
Jefferson Ribeiro <jefferson.silva@luizalabs.com>
2022-03-11 23:11:03 -0300

    refactor: just having fun

--- packages/admin/src/App.js
+++ packages/admin/src/App.js
@@ -1,13 +1,19 @@
 import { Button } from '@julius/common';
-import ButtonFederation from 'shareablebag/Button';
-import AppFederation from 'shareablebag/App';
+import ShareablebagButton from 'shareablebag/Button';
+import ShareablebagApp from 'shareablebag/App';
 
 function App() {
   return (
     <div>
-      <Button>Olá galera</Button>
-      <ButtonFederation />
-      <AppFederation />
+      <div style={{ backgroundColor: 'yellow' }}>
+        <p>Common app</p>
+        <Button>Olá galera</Button>
+      </div>
+      <div style={{ backgroundColor: 'grey' }}>
+        <p>Shareablebag app</p>
+        <ShareablebagButton />
+        <ShareablebagApp />
+      </div>
     </div>
   );
 }
--- packages/shareablebag/src/components/Button.js
+++ packages/shareablebag/src/components/Button.js
@@ -1,5 +1,5 @@
 const Button = () => {
-  return <button onClick={() => alert('minha nossa')}>Olá federation 2</button>;
+  return <button onClick={() => alert('minha nossa')}>Olá federation 1</button>;
 };
 
 export default Button;
