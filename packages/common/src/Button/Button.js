61a30b4b85f6ec03fe4bbc5044928854d9e06b58
Jefferson Ribeiro <jefferson.silva@luizalabs.com>
2022-03-15 15:33:26 -0300

    feat: change common button

--- packages/common/src/Button/Button.js
+++ packages/common/src/Button/Button.js
@@ -8,7 +8,7 @@ const Button = ({ children }) => {
   return (
     <button onClick={handleCounter} style={{ backgroundColor: 'red' }}>
       {children}
-      <span> Counter testando: {counter}</span>
+      <span> Counter testando Ramon: {counter}</span>
     </button>
   );
 };
